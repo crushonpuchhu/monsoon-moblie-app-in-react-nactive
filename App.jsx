d56b1278e03820c429139d0b0d92d59f16bd3df4
crushonpuchhu <mittalkumargupta@gmail.com>
2024-02-14 01:37:24 +0530

    second

--- App.jsx
+++ App.jsx
@@ -16,7 +16,6 @@ import Card from './src/Card';
 import Geolocation from 'react-native-geolocation-service';
 
 function App() {
-  const isdark = useColorScheme() === "dark"
   const [wData, SetWeatherdata] = useState(null);
   const [wDataForcast, SetWeatherdataForcast] = useState([]);
 
@@ -26,9 +25,9 @@ function App() {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         {
-          title: 'Cool Photo App Loction Permission',
+          title: 'App Loction Permission',
           message:
-            'Cool Loction App needs access to your Loction ',
+            'App needs access to your Loction ',
           buttonNeutral: 'Ask Me Later',
           buttonNegative: 'Cancel',
           buttonPositive: 'OK',
@@ -44,43 +43,47 @@ function App() {
     }
   };
 
-  useEffect( () => {
+  useEffect(() => {
     Permission();
-    Geolocation.getCurrentPosition(async(postion) => {
-      // console.log(postion.coords.latitude, postion.coords.longitude);
-      // console.log(postion)
-      try {
-        // current weather
-        const link1 = `https://api.openweathermap.org/data/2.5/weather?lat=${postion.coords.latitude}&lon=${postion.coords.longitude}&appid=a363d45a072022cd84fc736fee4e0bf7&units=metric`;
-        const WeatherData =  fetch(link1, {method: 'GET', headers: {'Content-Type': 'application/json;charset=utf-8'}})
-        
-        //  console.log(WeatherData);
-        //  forcast weather
-         const link2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${postion.coords.latitude}&lon=${postion.coords.longitude}&appid=a363d45a072022cd84fc736fee4e0bf7&units=metric`;
-         const WeatherDataforcast =  fetch(link2, {method: 'GET', headers: {'Content-Type': 'application/json;charset=utf-8'}})
-         
-         const res = await Promise.all([WeatherData,WeatherDataforcast])
-         const resfinal = await Promise.all([res[0].json(),res[1].json()])
-       
-           SetWeatherdata(resfinal[0]);
+    const time = setInterval(() => {
+
+      Geolocation.getCurrentPosition(async (postion) => {
+
+        try {
+          // current weather
+          const link1 = `https://api.openweathermap.org/data/2.5/weather?lat=${postion.coords.latitude}&lon=${postion.coords.longitude}&appid=a363d45a072022cd84fc736fee4e0bf7&units=metric`;
+          const WeatherData = fetch(link1, { method: 'GET', headers: { 'Content-Type': 'application/json;charset=utf-8' } })
+
+          //  console.log(WeatherData);
+          //  forcast weather
+          const link2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${postion.coords.latitude}&lon=${postion.coords.longitude}&appid=a363d45a072022cd84fc736fee4e0bf7&units=metric`;
+          const WeatherDataforcast = fetch(link2, { method: 'GET', headers: { 'Content-Type': 'application/json;charset=utf-8' } })
+
+          const res = await Promise.all([WeatherData, WeatherDataforcast])
+          const resfinal = await Promise.all([res[0].json(), res[1].json()])
+
+          SetWeatherdata(resfinal[0]);
 
 
-         const forcastArray = resfinal[1].list.filter((e,i)=>{
+
+          const forcastArray = resfinal[1].list.filter((e, i) => {
             return e.dt_txt.includes('03:00:00');
-           })
-          
-           forcastArray.shift()
-           SetWeatherdataForcast(forcastArray)
+          })
+
+          forcastArray.shift()
+          SetWeatherdataForcast(forcastArray)
 
 
-       } 
-       catch (error) {
-        console.log(error)
-       }
+        }
+        catch (error) {
+          console.log(error)
+        }
 
-    })
-  
+      })
 
+    }, 1000);
+
+    return () => { clearInterval(time) }
 
 
   }, [])
@@ -93,14 +96,14 @@ function App() {
       <View style={tw` flex  flex-row justify-start  items-center gap-10 h-[100px] w-[100%] `}>
         <Image style={{ width: 100, height: 100 }} source={require('./asset/cloud.png')} />
         <Text style={tw`font-normal text-5xl text-[#fefeff]`} >
-          {wData!==null?Math.trunc(wData.main.temp):"0"}°c
+          {wData !== null ? Math.trunc(wData.main.temp) : "0"}°c
         </Text>
       </View>
       {/* second box */}
       <View style={tw`w-[100%]  `}>
         <View style={tw`flex justify-start p-3 gap-2 items-end flex-row `}>
           <Text style={tw`font-bold  text-[32px] text-white tracking-wider `}>
-          {wData!==null?wData.name:"Serching..."}
+            {wData !== null ? wData.name : "Serching..."}
           </Text>
           <Text style={tw`text-emerald-800 py-1 font-bold text-[15px]`}>
             AM
@@ -109,7 +112,7 @@ function App() {
 
         <View style={tw` p-1  px-3`}>
           <Text style={tw` text-white font-bold`}>
-            Wind speed {wData!==null?Math.trunc(wData.wind.speed):"0"}km/h | Humidity: {wData!==null?Math.trunc(wData.main.humidity):"0"}%
+            Wind speed {wData !== null ? Math.trunc(wData.wind.speed) : "0"}km/h | Humidity: {wData !== null ? Math.trunc(wData.main.humidity) : "0"}%
           </Text>
         </View>
 
@@ -117,10 +120,10 @@ function App() {
       {/* third box */}
 
       <View style={tw`p-5 flex justify-center gap-3  bg-[#133fdd] bg-emerald-700 shadow-lg  rounded-3xl  w-[100%] `}>
-    
+
         {
-          wDataForcast.map((e,i)=>{
-            return <Card key={i} temp={e.main.temp} weatherStatus={e.weather.description} date={e.dt_txt} />
+          wDataForcast.map((e, i) => {
+            return <Card key={i} temp={e.main.temp} weatherStatus={e.weather[0].description} date={e.dt_txt} />
           })
         }
       </View>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { View,Text,Image } from "react-native";
+import { View, Text, Image } from "react-native";
 import tw from 'twrnc';
-export default function Card({temp,date,weatherStatus})
-{
-    const refineData=date.split(' ')[0].split('-')[2];
+export default function Card({ temp, date, weatherStatus }) {
+    const refineData = date.split(' ')[0].split('-')[2];
 
-    return(
-        <View   style={tw` justify-between  items-center  flex flex-row`}>
+    return (
+        <View style={tw` justify-between  items-center  flex flex-row`}>
             <Text style={tw` text-white text-[16px]`}>
                 date{refineData}
-                 {'\n'}
+                {'\n'}
                 {Math.trunc(temp)}° c
-                
+                {" " + weatherStatus}
             </Text>
-            <Image style={{ width: 70, height: 70 } } source={require('../asset/cloud.png')}/>
+            <Image style={{ width: 70, height: 70 }} source={require('../asset/cloud.png')} />
         </View>
     )
 }
